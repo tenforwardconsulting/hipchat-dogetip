6a749d645c5b232d9635291e0eda8c7f736f259b
Zach Boatrite <zach@tenforwardconsulting.com>
2014-02-15 17:38:52 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'haml-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'awesome_print'
+  gem 'pry'
+  gem 'rspec-rails'
+  gem 'spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HipchatDogetip::Application.config.secret_key_base = 'b893a8dde2b668dfc30d6aa997455341b001af033c80c20e9824ca09a5a4ac9f9d0799b7dd35d6b3a827638f7efc7ab27490828080540b9d27ddabb1e3edfca5'
